872b29434d806f65871c99fdbe9c733b28bbd83c
Sohil Khan <khansohil530@gmail.com>
2024-04-13 23:58:43 +0530

    feat: add close account

--- script.js
+++ script.js
@@ -176,3 +176,20 @@ btnTransfer.addEventListener("click", (e) => {
     updateUI(currentAccount);
   }
 });
+
+btnClose.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = "";
+});
